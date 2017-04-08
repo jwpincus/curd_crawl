c7c1aeb00d528233de47257b6f08bd21abef2560
Jack Pincus <jwpincus@gmail.com>
2017-04-08 15:08:48 -0600

    devise authentication working. Admin dash made

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,15 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
+  before_action :verify_is_admin, only: [:destroy]
   def create
     @review = Restaurant.find(review_params[:restaurant_id]).reviews.create(review_params)
     redirect_to restaurant_path(@review.restaurant)
+  end
+
+  def destroy
+    @restaurant = Review.find(params[:id]).restaurant
+    Review.find(params[:id]).destroy
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
@@ -10,4 +17,6 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:name, :review, :rating, :restaurant_id)
   end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:passwords]
   resources :restaurants
-  resources :reviews, only: [:create]
+  resources :reviews, only: [:create, :destroy]
+  get "/admin_dash", to: "users#admin_dash"
   root to: 'restaurants#index'
 end
